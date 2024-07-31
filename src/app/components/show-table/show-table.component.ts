3640fbd2dbc7a551d0091d2bcdda0616b4e84279
Devmithra Panwar Arya <dev87111@gmail.com>
2024-07-31 11:57:50 +0530

    added Text-to-Speech functionality

--- src/app/components/show-table/show-table.component.ts
+++ src/app/components/show-table/show-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-show-table',
@@ -9,5 +10,24 @@ import { NgFor, NgIf } from '@angular/common';
   styleUrl: './show-table.component.css'
 })
 export class ShowTableComponent {
-  @Input() productList: {id: number, selectedProduct: string, selectedQuantity: number}[] = []; 
+  @Input() productList: {id: number, selectedProduct: string, selectedQuantity: number}[] = [];
+  url : string = environment.apiUrl; 
+  apiKey : string = environment.apiKey;
+  order: string = '';
+  speakOrder(){
+    for(var i=0; i<this.productList.length; i++){
+      this.order = '';
+      if(this.productList[i].selectedProduct !== '' || this.productList[i].selectedQuantity !== 0){
+        this.order = this.order+"Order number "+this.productList[i].id;
+        this.order = this.order+" ,Product name "+this.productList[i].selectedProduct;
+        this.order = this.order+" ,Quantity "+this.productList[i].selectedQuantity+". ";
+      }
+    }
+    console.log(this.order);
+    new Audio(this.buildURL(this.order)).play();
+  }
+  buildURL(str : string){
+    return `${this.url}?key=${this.apiKey}&src=${str}&f=48khz_16bit_stereo`;
+  }
 }
+
